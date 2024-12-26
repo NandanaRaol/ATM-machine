a9562d2aeb59b2706f800cb57354a98c9ad1f9a4
NandanaRaol <142094872+NandanaRaol@users.noreply.github.com>
2024-12-26 11:13:36 +0530

    Create ATM.java

---
+++ ATM.java
@@ -0,0 +1,10 @@
+package project1;
+import java.io.IOException;
+public class ATM 
+{
+  public static void main(String args[]) throws IOException
+  {
+	  OptionMenu optionmenu=new OptionMenu();
+	  optionmenu.getLogin();
+  }
+}
